b125ac52ba7672ad6cc66ef7abb00616b643fd17
Fabio9791 <fabiofigueiredo9791@gmail.com>
2018-12-17 12:24:28 +0100

    view testing

--- src/Controller/ViewController.php
+++ src/Controller/ViewController.php
@@ -1,24 +1,30 @@
 <?php
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Competition;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ViewController extends AbstractController
+class ViewController extends Controller
 {
 
     /**
      *
      * @Route("/competition/view", name="view_competition")
      */
-    public function viewCompetition(Competition $competition)
+    public function viewCompetition(Request $request, ObjectManager $manager)
     {
-        $table = [];
+        
+        $competitionName = $request->attributes->get('name');
+        $competition = $manager->getRepository(Competition::class)->findOneByName($competitionName);
         $competitors = $competition->getCompetitors();
+        
+        $table = [];
         foreach ($competitors as $competitor) {
             array_push($table, [
-                $competitor,
+                $competitor->getName(),
                 0,
                 0,
                 0
@@ -26,14 +32,14 @@ class ViewController extends AbstractController
         }
         $encounters = $competition->getEncounters();
         foreach ($encounters as $encounter) {
-            $scores = $encounter->scores;
-            if ($scores[0]->score > $scores[1]->score) {
+            $scores = $encounter->getScores();
+            if ($scores[0]->getScore() > $scores[1]->getScore()) {
                 $points0 = 3;
                 $points1 = 0;
-            } elseif ($scores[0]->score < $scores[1]->score) {
+            } elseif ($scores[0]->getScore() < $scores[1]->getScore()) {
                 $points0 = 0;
                 $points1 = 3;
-            } elseif ($scores[0]->score === $scores[1]->score && $scores[1]->score != null) {
+            } elseif ($scores[0]->getScore() === $scores[1]->getScore() && $scores[1]->getScore() != null) {
                 $points0 = 1;
                 $points1 = 1;
             } else {
@@ -41,16 +47,16 @@ class ViewController extends AbstractController
                 $points1 = 0;
             }
             $comp0 = [
-                $scores[0]->competitorId,
+                $scores[0]->getCompetitorId()->getName(),
                 $points0,
-                $scores[0]->score,
-                $scores[1]->score
+                $scores[0]->getScore(),
+                $scores[1]->getScore()
             ];
             $comp1 = [
-                $scores[1]->competitorId,
+                $scores[1]->getCompetitorId()->getName(),
                 $points1,
-                $scores[1]->score,
-                $scores[0]->score
+                $scores[1]->getScore(),
+                $scores[0]->getScore()
             ];
 
             for ($i = 0; $i < sizeof($table); $i ++) {
@@ -69,7 +75,11 @@ class ViewController extends AbstractController
 
         for ($i = 0; $i < sizeof($table) - 1; $i ++) {
             for ($j = $i + 1; $j < sizeof($table); $j ++) {
-                if ($table[$i][1] < $table[$j][1] || ($table[$i][1] === $table[$j][1] && ($table[$i][2] - $table[$i][3] < $table[$j][2] - $table[$j][3] || ($table[$i][2] - $table[$i][3] === $table[$j][2] - $table[$j][3] && $table[$i][2] === $table[$j][2])))) {
+                if ($table[$i][1] < $table[$j][1] || 
+                    ($table[$i][1] === $table[$j][1] && 
+                        ($table[$i][2] - $table[$i][3] < $table[$j][2] - $table[$j][3] || 
+                            ($table[$i][2] - $table[$i][3] === $table[$j][2] - $table[$j][3] && 
+                                $table[$i][2] === $table[$j][2])))) {
                     $temp = $table[$i];
                     $table[$i] = $table[$j];
                     $table[$j] = $temp;
@@ -80,7 +90,7 @@ class ViewController extends AbstractController
         $matchDays = $competition->getMatchDays();
         for ($y = 0; $y < sizeof($matchDays) - 1; $y ++) {
             for ($i = $y + 1; $i < sizeof($matchDays); $i ++) {
-                if (intval(substr($matchDays[$y]->getLabel, sizeof($matchDays[$y]->getLabel) - 1)) > intval(substr($matchDays[$i]->getLabel, sizeof($matchDays[$i]->getLabel) - 1))) {
+                if (intval(substr($matchDays[$y]->getLabel(), strlen($matchDays[$y]->getLabel()) - 1)) > intval(substr($matchDays[$i]->getLabel(), strlen($matchDays[$i]->getLabel()) - 1))) {
                     $temp = $matchDays[$y];
                     $matchDays[$y] = $matchDays[$i];
                     $matchDays[$i] = $temp;
@@ -91,14 +101,16 @@ class ViewController extends AbstractController
         $table2 = [];
         for ($i = 0; $i < sizeof($matchDays); $i ++) {
             array_push($table2, [
-                $matchDays[$i]->getLabel
+                $matchDays[$i]->getLabel()
             ]);
-            $encounters = $matchDays[$i]->encounter;
+            $encounters = $matchDays[$i]->getEncounter();
+            $encountersArray=[];
             for ($j = 0; $j < sizeof($encounters); $j ++) {
-                $scores = $encounters[$j]->scores;
-                $encounter = [$scores[0]->getCompetitorId, $scores[0]->score , $scores[1]->score, $scores[1]->getCompetitorId];
+                $scores = $encounters[$j]->getScores();
+                $encounter = [$scores[0]->getCompetitorId()->getName(), $scores[0]->getScore() , $scores[1]->getScore(), $scores[1]->getCompetitorId()->getName()];
+                array_push($encountersArray,$encounter);
             }
-            array_push($table2[$i],[$encounter]);
+            array_push($table2[$i],$encountersArray);
         }
 
         return $this->render('Competition/view.html.twig', [
